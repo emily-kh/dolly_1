09d869d2004ea7d8b3e720a3328fff779b2faf04
emily-kh <kitaharaemiri722@gmail.com>
2021-07-05 17:14:17 +0900

    詳細ページ１

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
   get 'users/index'
-  resources :items, only:[:index, :new, :create]
+  resources :items, only:[:index, :new, :createm, :show]
   resources :owners, only: :index
 end
