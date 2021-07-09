468b5f73e6b377ee590c2c61dfd34e9d46b0deaf
emily-kh <kitaharaemiri722@gmail.com>
2021-07-09 20:22:32 +0900

    編集・削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show,:edit, :update, :destroy]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -17,12 +20,31 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name,:description,:price, :category_id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
   get 'users/index'
-  resources :items, only:[:index, :new, :createm, :show]
+  resources :items
   resources :owners, only: :index
 end
